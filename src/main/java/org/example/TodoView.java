03a031e3aafb9c0df893444816b30d6e48a00205
Nhan Tran <rim25102004@gmail.com>
2023-09-25 23:12:21 +0300

    Nhan commit View

--- src/main/java/org/example/TodoView.java
+++ src/main/java/org/example/TodoView.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import java.util.List;
 public class TodoView {
+    public void showTasks(List<String> tasks) {
+        System.out.println("To-Do List:");
+        int index = 1;
+        for (String task : tasks) {
+            System.out.println(index + ". " + task);
+            index++;
+        }
+    }
 }
