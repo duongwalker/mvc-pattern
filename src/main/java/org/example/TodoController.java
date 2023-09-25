c17ced7822c3c2e50c8f11e33de0f17bcabe62e4
Chu Hai <duchaichu1303@gmail.com>
2023-09-26 02:27:40 +0300

    ad hoc solution

--- src/main/java/org/example/TodoController.java
+++ src/main/java/org/example/TodoController.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import java.util.List;
+
+/**
+ * TodoController is used to handle requests from users sent via TodoView
+ */
+
 public class TodoController {
+    TodoModel todoModel;
+    TodoView todoView;
+    TodoController(TodoModel todoModel, TodoView todoView) {
+        this.todoModel = todoModel;
+        this.todoView = todoView;
+    }
+    // Add new task(s) to the list
+    void addTasks(String... tasks) {
+        for (String task : tasks) {
+            todoModel.addTask(task);
+        }
+    }
+    // Remove task (should be called when a task is marked as done)
+    void removeTask(String task) throws NullPointerException {
+        todoModel.removeTask(task);
+    }
+    List<String> getTasks() {
+        return todoModel.getTasks();
+    }
 }
